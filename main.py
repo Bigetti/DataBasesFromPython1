3b4fe24d65c45027c7b87253e19a94fd8fbb5be8
bigetti <upgrade81@mail.ru>
2023-09-27 16:01:38 +0300

    homework postgress SQL from python stage2

--- main.py
+++ main.py
@@ -19,38 +19,73 @@ def create_database_structure(conn):
         CREATE TABLE IF NOT EXISTS phones (
             phone_id SERIAL PRIMARY KEY,
             client_id INT REFERENCES clients(client_id),
-            phone_number VARCHAR(20)
+            phone_number VARCHAR(20) UNIQUE
         );
         """)
     conn.commit()
 
-
+#Добавляем нового клиента
 def add_client(conn, first_name, last_name, email):
     with conn.cursor() as cur:
-        cur.execute ("INSERT INTO clients (first_name, last_name, email) VALUES (%s, %s, %s) RETURNING client_ID, first_name, last_name, email")
-        client_id = cur.fetchone()[0]
-    
-    conn.commit()
+        # Проверяем, есть ли клиент с таким именем и фамилией в базе данных
+        cur.execute("SELECT client_id FROM clients WHERE first_name = %s AND last_name = %s", (first_name, last_name))
+        existing_client = cur.fetchone()
 
+        if existing_client:
+            # Клиент с таким именем и фамилией уже существует, вернуть его идентификатор
+            return existing_client[0]
+
+        # Вставляем нового клиента, так как его имя и фамилия не совпадают с существующими
+        cur.execute("INSERT INTO clients (first_name, last_name, email) VALUES (%s, %s, %s) RETURNING client_ID", (first_name, last_name, email))
+        client_id = cur.fetchone()[0]
+
+    conn.commit()
     return client_id
 
-def new_phone_number_for_client(conn, client_id, phone_number):
+
+
+def add_phone_number_for_client(conn, client_id, phone_number):
     with conn.cursor() as cur:
-        # Вставляем телефон для клиента в таблицу phones
-        cur.execute("INSERT INTO phones (client_id, phone_number) VALUES (%s, %s)", (client_id, phone_number))
+        # Проверяем, существует ли клиент с указанным client_id
+        cur.execute("SELECT 1 FROM clients WHERE client_id = %s", (client_id,))
+        existing_client = cur.fetchone()
+
+        if existing_client:
+            # Если клиент существует, то проверяем, есть ли уже такой номер телефона
+            cur.execute("SELECT 1 FROM phones WHERE client_id = %s AND phone_number = %s::VARCHAR", (client_id, str(phone_number)))
+            existing_entry = cur.fetchone()
+
+            if not existing_entry:
+                # Если записи не существует, то вставляем номер телефона
+                cur.execute("INSERT INTO phones (client_id, phone_number) VALUES (%s, %s::VARCHAR)", (client_id, str(phone_number)))
 
     conn.commit()
 
-
-    
-def change_client_data(conn, client_id, first_name, last_name, email):
+#Меняем некоторые данные клиента кроме телефона
+def change_client_data(conn, client_id, new_first_name=None, new_last_name=None, new_email=None):
     with conn.cursor() as cur:
+        query = "UPDATE clients SET "
+        params = []
 
-        cur.execute("UPDATE clients SET first_name = %s, last_name = %s, email = %s WHERE client_id = %s", (first_name, last_name, email, client_id))
+        if new_first_name is not None:
+            query += "first_name = %s, "
+            params.append(new_first_name)
+        if new_last_name is not None:
+            query += "last_name = %s, "
+            params.append(new_last_name)
+        if new_email is not None:
+            query += "email = %s, "
+            params.append(new_email)
+
+        # Удаляем последнюю запятую и добавляем условие WHERE
+        query = query.rstrip(", ") + " WHERE client_id = %s"
+        params.append(client_id)
+
+        cur.execute(query, params)
 
     conn.commit()
 
-
+#Удаляем данные о телефоне клиента  
 def delete_phone_number_for_client(conn, phone_id):
     with conn.cursor() as cur:
 
@@ -58,26 +93,32 @@ def delete_phone_number_for_client(conn, phone_id):
 
     conn.commit()
 
-
-def delete_client (conn, client_id):
+#Удаляем  клиента  
+def delete_client(conn, client_id):
     with conn.cursor() as cur:
-    
-        cur.execute("DELETE FROM CLIENT WHERE client_id = %s", (client_id,))
+        try:
+            # Удаляем записи о телефонах клиента
+            cur.execute("DELETE FROM phones WHERE client_id = %s", (client_id,))
+            
+            # Теперь, после удаления связанных записей, можно удалить самого клиента
+            cur.execute("DELETE FROM clients WHERE client_id = %s", (client_id,))
+        except Exception as e:
+            conn.rollback()
+            raise e
 
     conn.commit()
                 
-
-def find_client (conn, search_string):
+# Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону.
+def find_client(conn, search_string):
     with conn.cursor() as cur:
-         # Ищем клиента по имени, фамилии, email или телефону
         cur.execute("""
-        SELECT clients.* 
+        SELECT DISTINCT clients.client_id, first_name, last_name, email
         FROM clients
         LEFT JOIN phones ON clients.client_id = phones.client_id
-        WHERE first_name = %s 
-        OR last_name = %s 
-        OR email = %s 
-        OR phone_number = %s
+        WHERE first_name LIKE %s 
+        OR last_name LIKE %s 
+        OR email LIKE %s 
+        OR phone_number LIKE %s
         """, ('%' + search_string + '%', '%' + search_string + '%', '%' + search_string + '%', '%' + search_string + '%'))
         result = cur.fetchall()
     return result
@@ -86,12 +127,13 @@ def find_client (conn, search_string):
 if __name__ == "__main__": 
 
  # Создаем соединение с базой данных
-    conn = psycopg2.connect(database="clients", user="postgress", password="Goblin000", host="localhost", port="5432")
+    conn = psycopg2.connect(database="clients", user="postgres", password="Samsung000", host="localhost", port="5432")
+    
 
     # Создаем структуру базы данных
     create_database_structure(conn)
 
-    # Добавляем нового клиента
+    # # Добавляем нового клиента
     new_client_id = add_client(conn, 'John', 'Doe', 'johndoe@example.com')
 
     
@@ -104,8 +146,46 @@ if __name__ == "__main__":
     new_client_id = add_client(conn, 'Sam', 'Wayn', 'SamWayn@example.com')
 
 
+    # Добавляем номера телефонов новым клиентам
+    new_phone_number = add_phone_number_for_client(conn, 1, 89112221111)
 
-    new_phone_number = new_phone_number_for_client(conn, 1, 89112221111)
+    new_phone_number = add_phone_number_for_client(conn, 1, 891122222222)
+
+    new_phone_number = add_phone_number_for_client(conn, 2, 89133333333)
+    
+    new_phone_number = add_phone_number_for_client(conn, 3, 8914444444)
+
+
+    # # Меняем данные клиента
+    # change_client_data(conn, 1, 'Vasya', 'Petrov', None,)
+
+    # change_client_data(conn, 2, 'Vasya', None, 'Petrov@test.ru')
+
+
+
+    # #Удаляем данные о телефоне клиента  
+    # delete_phone_number_for_client(conn, 3)
+
+    # # delete_phone_number_for_client(conn, 2)
+
+
+
+    # #Удаляем  клиента  
+    # delete_client (conn, 1)
+
+
+
+ # ищем клиента по его данным: имени, фамилии, email или телефону.
+    
+    results = find_client (conn, 'John')
+    print("Результаты поиска:")
+    if results:
+        print("Результаты поиска:")
+        for result in results:
+            print(result)
+    else:
+        print("Клиент не найден.")
+
 
 
 
